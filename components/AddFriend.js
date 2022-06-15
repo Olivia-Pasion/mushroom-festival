c9be2ded62fa83adfb6649c941f8b0d8524ef310
olivia pasion <oap9820@gmail.com>
2022-06-15 14:07:53 -0700

    Add friend

--- components/AddFriend.js
+++ components/AddFriend.js
@@ -6,8 +6,11 @@ export default function createAddFriend(root) {
 
     form.addEventListener('submit', (e) => {
         // *** prevent the form's default behavior of changing the browser page
+        e.preventDefault();
         // create a formData object
+        const formData = new FormData(form);
         // call the handler with the "name" value from the formData
+        handleAddFriend(formData.get('name'));
     });
 
     return () => { };
